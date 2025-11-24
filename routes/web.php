9196e00c1dee214dc103f51c05061d93d2ddd5e6
Karim Nabil <karimnabil290@gmail.com>
2025-11-24 15:13:08 +0200

    Adding user authentation but not tested

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth','role:patient'])->group(function(){
+    Route::get('/patient/dashboard',[PatientController::class,'dashboard'])->name('patient.dashboard');
+});
+
+Route::middleware(['auth','role:doctor'])->group(function(){
+    Route::get('/doctor/dashboard',[DoctorController::class,'dashboard'])->name('doctor.dashboard');
+});
+
+Route::middleware(['auth','role:admin'])->group(function(){
+    Route::get('/admin/dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
+});
+
+require __DIR__.'/auth.php';
